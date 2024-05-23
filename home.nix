34d88da96520a4533c88d69bda61cda06000ee1e
shimo8810 <shimo.8810@gmail.com>
2024-05-23 22:25:27 +0900

    feat: update bat theme

--- home.nix
+++ home.nix
@@ -18,7 +18,6 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    bat
     bottom
     duf
     dust
@@ -139,5 +138,10 @@
 
   programs.alacritty = {
     enable = true;
+  };
+
+  programs.bat = {
+    enable = true;
+    config.theme = "Nord";
   };
 }
