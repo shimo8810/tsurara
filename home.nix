57c02dcf0b1fbef2d9d72d1ffb501f87278d9bd2
shimo8810 <shimo.8810@gmail.com>
2024-05-26 17:53:37 +0900

    test: check home.file

--- home.nix
+++ home.nix
@@ -50,7 +50,7 @@
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+    ".hogepiyo".source = dotfiles/hogepiyo;
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
