3cd9f5f3c0dfbac7392c995f43418e6068a61644
shimo8810 <shimo.8810@gmail.com>
2024-05-26 13:37:38 +0900

    feat: update zsh

--- home.nix
+++ home.nix
@@ -113,24 +113,39 @@
   # zsh configuration
   programs.zsh = {
     enable = true;
-    enableCompletion = true;
+    # enableCompletion = true;
     # autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
+    # syntaxHighlighting.enable = true;
     shellAliases = {
-      ll = "ls -l";
-  };
-  zplug = {
-    enable = true;
-    plugins = [
-      { name = "zsh-users/zsh-autosuggestions"; }
-      { name = "zsh-users/zsh-syntax-highlighting"; }
-      { name = "zsh-users/zsh-completions"; }
-      { name = "mafredri/zsh-async"; }
-      { name = "sindresorhus/pure"; }
-    ];
-  };
-  # histSize = 10000;
-  # histFile = "${config.xdg.dataHome}/zsh/history";
+      ls = "lsd";
+      cat = "bat";
+      grep = "rg";
+    };
+    dotDir = ".config/zsh";
+    zplug = {
+      enable = true;
+      plugins = [
+        { name = "zsh-users/zsh-autosuggestions"; }
+        {
+          name = "zsh-users/zsh-syntax-highlighting";
+          tags = [ "defer:2" ];
+         }
+        { name = "zsh-users/zsh-completions"; }
+        {
+          name = "mafredri/zsh-async";
+          tags = [ "from:github" ];
+        }
+        {
+          name = "sindresorhus/pure";
+          tags = [ "use:pure.zsh" "from:github" "as:theme" ];
+        }
+      ];
+    };
+    history.size = 100000;
+    initExtra = ''
+      autoload -U promptinit; promptinit
+      zstyle :prompt:pure:git:stash show yes
+    '';
   };
 
   programs.alacritty = {
