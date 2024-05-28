b32b5e3dab3d49ea2271078549acffc7b129a7c3
shimo8810 <shimo.8810@gmail.com>
2024-05-28 23:02:19 +0900

    feat: add tmux support

--- home/tmux/default.nix
+++ home/tmux/default.nix
@@ -1,6 +1,20 @@
 { pkgs, lib, ... }: {
-    programs.tmux = {
-        enable = true;
-        extraConfig = lib.fileContents ./tmux.conf;
-    };
+  programs.tmux = {
+    enable = true;
+    clock24 = true;
+    escapeTime = 0;
+    historyLimit = 5000;
+    mouse = true;
+    keyMode = "vi";
+    prefix = "C-a";
+    terminal = "screen-256color";
+    tmuxinator.enable = true;
+    plugins = with pkgs; [
+      {
+        plugin = tmuxPlugins.nord;
+        extraConfig = "set -g @plugin 'nordtheme/tmux'";
+      }
+    ];
+    extraConfig = lib.fileContents ./tmux.conf;
+  };
 }
