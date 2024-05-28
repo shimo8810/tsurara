0e0b7b6724918198383edf3c085eea06904b6d6d
shimo8810 <shimo.8810@gmail.com>
2024-05-28 20:32:41 +0900

    feat: merge nixos and dotfiles

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Home Manager configuration";
+  description = "dotfiles and nixos configuration";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -7,18 +7,40 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    xremap-flake.url = "github:xremap/nix-flake";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }:
-    let
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        config.allowUnfree = true;
-      };
-    in
+  outputs = inputs@{ nixpkgs, home-manager, xremap-flake, ... }:
     {
+      nixosConfigurations."pipkrake" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          xremap-flake.nixosModules.default
+          {
+            services.xremap = {
+              serviceMode = "system";
+              config = {
+                modmap = [
+                  {
+                    name = "capslock to control";
+                    remap.CapsLock = "Ctrl_L";
+                  }
+                ];
+              };
+            };
+          }
+        ];
+        specialArgs = {
+          inherit inputs;
+        };
+      };
+
       homeConfigurations."home_x64" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+        };
         extraSpecialArgs = {
           inherit inputs;
         };
