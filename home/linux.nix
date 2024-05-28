bade714e1254087d0e9d5905df0eb664a617ef99
shimo8810 <shimo.8810@gmail.com>
2024-05-28 21:59:51 +0900

    feat: divide zsh files

--- home/linux.nix
+++ home/linux.nix
@@ -56,47 +56,6 @@
     };
   };
 
-  # zsh configuration
-  programs.zsh = {
-    enable = true;
-    # enableCompletion = true;
-    # autosuggestion.enable = true;
-    # syntaxHighlighting.enable = true;
-    shellAliases = {
-      ls = "lsd";
-      cat = "bat";
-      grep = "rg";
-    };
-    dotDir = ".config/zsh";
-    zplug = {
-      enable = true;
-      plugins = [
-        { name = "zsh-users/zsh-autosuggestions"; }
-        {
-          name = "zsh-users/zsh-syntax-highlighting";
-          tags = [ "defer:2" ];
-        }
-        { name = "zsh-users/zsh-completions"; }
-        {
-          name = "mafredri/zsh-async";
-          tags = [ "from:github" ];
-        }
-        {
-          name = "sindresorhus/pure";
-          tags = [ "use:pure.zsh" "from:github" "as:theme" ];
-        }
-      ];
-    };
-    history.size = 100000;
-    initExtra = ''
-      autoload -U promptinit; promptinit
-      zstyle :prompt:pure:git:stash show yes
-      zstyle ':completion:*' menu select
-    '';
-  };
-
-  programs.alacritty.enable = true;
-
   programs.bat = {
     enable = true;
     config.theme = "Nord";
