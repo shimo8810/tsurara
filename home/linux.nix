7e04007fd28843414b3e1f6695698c303a837b38
shimo8810 <shimo.8810@gmail.com>
2024-05-28 22:04:00 +0900

    style: nixpkgs-fmt

--- home/linux.nix
+++ home/linux.nix
@@ -1,19 +1,11 @@
 { config, pkgs, lib, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should manage.
   home.username = "shimo";
   home.homeDirectory = "/home/shimo";
 
-  # This value determines the Home Manager release that your configuration is compatible with.
-  # This helps avoid breakage when a new Home Manager release introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager.
-  # If you do want to update the value, then make sure to first check the Home Manager release notes.
-  # Please read the comment before changing.
   home.stateVersion = "24.05";
 
-  # The home.packages option allows you to install Nix packages into your environment.
   home.packages = with pkgs; [
     bottom
     duf
@@ -28,19 +20,10 @@
     ripgrep
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage plain files is through 'home.file'.
-  home.file = {
-    # ".config/alacritty/alacritty.toml".source = ./alacritty/alacritty.toml;
-  };
+  home.file = { };
 
-  # Home Manager can also manage your environment variables through 'home.sessionVariables'.
-  # These will be explicitly sourced when using a shell provided by Home Manager.
-  # If you don't want to manage your shell through Home Manager then you have to manually source 'hm-session-vars.sh'
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
+  home.sessionVariables = { };
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
   programs.bat = {
