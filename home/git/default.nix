7acc32224b1d814d57f5b4d4819855d9f6fccca5
shimo8810 <shimo.8810@gmail.com>
2024-06-03 22:37:49 +0900

    feat: add support git delta

--- home/git/default.nix
+++ home/git/default.nix
@@ -4,7 +4,14 @@
     enable = true;
     userName = "shimo8810";
     userEmail = "shimo.8810@gmail.com";
-
+    delta.enable = true;
+    delta.options = {
+      line-numbers = true;
+      side-by-side = true;
+      dark = true;
+      syntax-theme = "Nord";
+      keep-plus-minus-markers = true;
+    };
     extraConfig = {
       core.editor = "nvim";
       init.defaultBranch = "main";
