4b69def438eda47686cde89ac51cbc9c23e85619
shimo8810 <shimo.8810@gmail.com>
2024-06-05 13:49:50 +0900

    chore: update alacritty configuration

--- home/alacritty/default.nix
+++ home/alacritty/default.nix
@@ -1,9 +1,60 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [
-    alacritty
-  ];
+{ ... }: {
 
-  home.file = {
-    ".config/alacritty/alacritty.toml".source = ./alacritty.toml;
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      window = {
+        opacity = 0.85;
+        blur = false;
+      };
+
+
+      font = {
+        size = 16;
+        normal = {
+          family = "HackGen35 Console NF";
+          style = "Regular";
+        };
+      };
+
+      colors = {
+        primary = {
+          background = "#0A0C0F";
+          foreground = "#D8DEE9";
+        };
+
+        normal = {
+          black = "#3B4252";
+          red = "#BF616A";
+          green = "#A3BE8C";
+          yellow = "#EBCB8B";
+          blue = "#81A1C1";
+          magenta = "#B48EAD";
+          cyan = "#88C0D0";
+          white = "#E5E9F0";
+        };
+
+        bright = {
+          black = "#4C566A";
+          red = "#BF616A";
+          green = "#A3BE8C";
+          yellow = "#EBCB8B";
+          blue = "#81A1C1";
+          magenta = "#B48EAD";
+          cyan = "#8FBCBB";
+          white = "#ECEFF4";
+        };
+      };
+
+      cursor.style = {
+        shape = "Beam";
+        blinking = "Always";
+      };
+
+      keyboard.bindings = [
+        { key = "F11"; action = "ToggleFullscreen"; }
+      ];
+
+    };
   };
 }
