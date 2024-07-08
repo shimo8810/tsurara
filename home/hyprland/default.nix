cefe362c9389d681362be0be748300dab7967b81
shimo8810 <shimo.8810@gmail.com>
2024-07-08 23:31:51 +0900

    feat: add config of hyprland

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -1,4 +1,6 @@
 { pkgs, ... }: {
+  wayland.windowManager.hyprland = {
+    # Whether to enable Hyprland wayland compositor
     enable = true;
     # The hyprland package to use
     package = pkgs.hyprland;
@@ -8,4 +10,21 @@
     # Optional
     # Whether to enable hyprland-session.target on hyprland startup
     systemd.enable = true;
+  };
+
+  wayland.windowManager.hyprland.settings = {
+    decoration = {
+      shadow_offset = "0 5";
+      "col.shadow" = "rgba(00000099)";
+    };
+
+    "$mod" = "SUPER";
+
+    bindm = [
+      # mouse movements
+      "$mod, mouse:272, movewindow"
+      "$mod, mouse:273, resizewindow"
+      "$mod ALT, mouse:272, resizewindow"
+    ];
+  };
 }
