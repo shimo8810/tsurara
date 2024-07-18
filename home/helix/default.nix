7d8324c4679628858f7cdb4413fc786d150c3ccc
shimo8810 <shimo.8810@gmail.com>
2024-07-18 22:04:46 +0900

    feat: add helix config

--- home/helix/default.nix
+++ home/helix/default.nix
@@ -1,9 +1,29 @@
 { pkgs, ... }: {
   programs.helix = {
     enable = true;
+
+    settings = {
+      theme = "nord-night";
+      editor = {
+        true-color = true;
+        cursorline = true;
+        color-modes = true;
+        auto-completion = true;
+        auto-save = true;
+      };
+
+      editor.whitespace.render = {
+        space = "all";
+        tab = "all";
+        newline = "all";
+      };
+
+      editor.indent-guides = {
+        render = true;
+        character = "･";
+      };
+    };
   };
 
-  # home.file = {
-  #   ".config/zellij/config.kdl".source = ./config.kdl;
-  # };
+
 }
