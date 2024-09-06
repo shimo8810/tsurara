7bcc0015998e6ae41af65e97671d156cbb33a4d7
shimo8810 <shimo.8810@gmail.com>
2024-09-06 13:10:59 +0900

    refactor: remove comment

--- hosts/hemingway/home.nix
+++ hosts/hemingway/home.nix
@@ -1,17 +1,5 @@
-{ pkgs, inputs, ... }:
-
+{ ... }:
 {
-  # home.username = "shimo";
-  # home.homeDirectory = "/home/shimo";
-
-  # home.stateVersion = "24.05";
-
-  # home.file = { };
-
-  # home.sessionVariables = { };
-
-  # programs.home-manager.enable = true;
-
   imports = [
     ../../home/cli
     ../../home/desktop
