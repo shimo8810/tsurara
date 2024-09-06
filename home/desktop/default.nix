b0ead37e2c0905c3714ea18c6b47e940c3a049bf
shimo8810 <shimo.8810@gmail.com>
2024-09-06 11:39:59 +0900

    feat: change dir

---
+++ home/desktop/default.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    nordic
+    candy-icons
+    tela-circle-icon-theme
+    orchis-theme
+    graphite-gtk-theme
+    tokyonight-gtk-theme
+    zafiro-icons
+    nordzy-icon-theme
+    nordzy-cursor-theme
+    vimix-cursors
+    hackgen-nf-font
+  ];
+}
