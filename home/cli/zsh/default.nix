133993ef7ec68ec6b96dda6fb76d9045d4852bc0
shimo8810 <shimo.8810@gmail.com>
2024-09-15 23:20:35 +0900

    feat: enable starship and add settings

--- home/cli/zsh/default.nix
+++ home/cli/zsh/default.nix
@@ -19,14 +19,55 @@
     # initExtra = lib.fileContents ./zshrc;
   };
 
-  # shelldon, zsh package manager
-  # home.packages = with pkgs; [
-  #   sheldon
-  # ];
+  programs.starship = {
+    enable = true;
+    settings = builtins.fromTOML ''
+      format = """
+      $username\
+      $hostname\
+      $directory\
+      $git_branch\
+      $git_state\
+      $git_status\
+      $cmd_duration\
+      $line_break\
+      $python\
+      $character"""
 
-  # home.file = {
-  #   ".config/sheldon/plugins.toml".source = ./plugins.toml;
-  # };
+      [directory]
+      style = "blue"
 
-  programs.starship.enable = true;
+      [character]
+      success_symbol = "[❯](purple)"
+      error_symbol = "[❯](red)"
+      vimcmd_symbol = "[❮](green)"
+
+      [git_branch]
+      format = "[$branch]($style)"
+      style = "bright-black"
+
+      [git_status]
+      format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)"
+      style = "cyan"
+      conflicted = "​"
+      untracked = "​"
+      modified = "​"
+      staged = "​"
+      renamed = "​"
+      deleted = "​"
+      stashed = "≡"
+
+      [git_state]
+      format = '\([$state( $progress_current/$progress_total)]($style)\) '
+      style = "bright-black"
+
+      [cmd_duration]
+      format = "[$duration]($style) "
+      style = "yellow"
+
+      [python]
+      format = "[$virtualenv]($style) "
+      style = "bright-black"
+    '';
+  };
 }
