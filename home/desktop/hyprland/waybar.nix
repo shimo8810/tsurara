1acbfda03f9f10c840579cf77a44c68fa1e03760
shimo8810 <shimo.8810@gmail.com>
2024-09-20 23:57:48 +0900

    feat: add waybar settings

--- home/desktop/hyprland/waybar.nix
+++ home/desktop/hyprland/waybar.nix
@@ -1,7 +1,28 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.waybar = {
     enable = true;
     systemd.enable = true;
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+        height = 30;
+        modules-left = [
+          "sway/workspaces"
+          "sway/mode"
+          "wlr/taskbar"
+        ];
+        modules-center = [
+          "sway/window"
+          "custom/hello-from-waybar"
+        ];
+        modules-right = [
+          "mpd"
+          "custom/mymodule#with-css-id"
+          "temperature"
+        ];
+      };
+    };
   };
 }
