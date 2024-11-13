787ef5718aeecba06018f562e6a52ce2934c3acc
shimo8810 <shimo.8810@gmail.com>
2024-11-13 15:09:23 +0900

    feat: enable xsession and configure nixGL for NVIDIA support

--- hosts/hemingway/home.nix
+++ hosts/hemingway/home.nix
@@ -9,6 +9,20 @@
 {
   home.homeDirectory = "/home/${username}";
 
+  xsession.enable = true;
+
+  nixGL = {
+    packages = inputs.nixgl.packages;
+    # defaultWrapper = "nvidiaPrime";
+    defaultWrapper = "nvidia";
+    offloadWrapper = "nvidiaPrime";
+    installScripts = [
+      "nvidia"
+      "nvidiaPrime"
+    ];
+  };
+
+
   imports = [
     ../../home/cli
     ../../home/desktop
