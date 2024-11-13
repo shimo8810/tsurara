e4776522a94798c953e18fc285c66e048134b4b2
shimo8810 <shimo.8810@gmail.com>
2024-11-13 15:09:32 +0900

    feat: add alacritty configuration with custom settings and key bindings

--- hosts/hemingway/home.nix
+++ hosts/hemingway/home.nix
@@ -22,6 +22,66 @@
     ];
   };
 
+  programs.alacritty = {
+    enable = true;
+    package = config.lib.nixGL.wrap pkgs.alacritty;
+    settings = {
+      window = {
+        opacity = 0.5;
+        blur = false;
+      };
+
+      font = {
+        size = 10;
+        normal = {
+          family = "HackGen35 Console NF";
+          style = "Regular";
+        };
+      };
+
+      colors = {
+        primary = {
+          background = "#0A0C0F";
+          foreground = "#D8DEE9";
+        };
+
+        normal = {
+          black = "#3B4252";
+          red = "#BF616A";
+          green = "#A3BE8C";
+          yellow = "#EBCB8B";
+          blue = "#81A1C1";
+          magenta = "#B48EAD";
+          cyan = "#88C0D0";
+          white = "#E5E9F0";
+        };
+
+        bright = {
+          black = "#4C566A";
+          red = "#BF616A";
+          green = "#A3BE8C";
+          yellow = "#EBCB8B";
+          blue = "#81A1C1";
+          magenta = "#B48EAD";
+          cyan = "#8FBCBB";
+          white = "#ECEFF4";
+        };
+      };
+
+      cursor.style = {
+        shape = "Beam";
+        blinking = "Always";
+      };
+
+      keyboard.bindings = [
+        {
+          key = "F11";
+          action = "ToggleFullscreen";
+        }
+      ];
+
+    };
+  };
 
   imports = [
     ../../home/cli
