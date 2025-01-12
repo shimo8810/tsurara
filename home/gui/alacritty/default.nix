c1fcc47e70bb739cde0c72cfe33ec4d81eb5fe6a
shimo8810 <shimo.8810@gmail.com>
2025-01-13 01:04:35 +0900

    feat: wrap alacritty package with nixGL

--- home/gui/alacritty/default.nix
+++ home/gui/alacritty/default.nix
@@ -1,8 +1,9 @@
-{ ... }:
+{ config, pkgs, ... }:
 {
 
   programs.alacritty = {
     enable = true;
+    package = config.lib.nixGL.wrap pkgs.alacritty;
     settings = {
       window = {
         opacity = 0.5;
