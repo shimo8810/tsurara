e906e75f051468437e30fff42b7ccaf2bab40538
shimo8810 <shimo.8810@gmail.com>
2025-02-16 12:38:32 +0900

    feat: re-enable icons

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -3,13 +3,13 @@
   home.packages = with pkgs; [
     nordic
     candy-icons
-    # tela-circle-icon-theme
+    tela-circle-icon-theme
     orchis-theme
     graphite-gtk-theme
     tokyonight-gtk-theme
     zafiro-icons
-    # nordzy-icon-theme
-    # nordzy-cursor-theme
+    nordzy-icon-theme
+    nordzy-cursor-theme
     vimix-cursors
     hackgen-nf-font
     capitaine-cursors-themed
