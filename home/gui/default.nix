26a407d2b4b063eaac34daa9e30c969cc0c399ea
shimo8810 <shimo.8810@gmail.com>
2025-05-12 20:35:04 +0900

    feat: comment out unused packages in default.nix

--- home/gui/default.nix
+++ home/gui/default.nix
@@ -1,15 +1,19 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   home.packages = with pkgs; [
-    brave
-    firefox
-    vscode
-    discord
-    slack
+    # brave
+    # firefox
+    # vscode
+    # discord
+    # slack
+    (config.lib.nixGL.wrap obsidian)
   ];
 
   imports = [
     ./alacritty
+    ./browser.nix
+    ./cad.nix
+    ./editor.nix
     ./ghostty
   ];
 }
