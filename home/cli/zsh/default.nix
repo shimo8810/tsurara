12f9412d9bd08518455105f50c9abf43b208fd29
shimo8810 <shimo.8810@gmail.com>
2025-07-15 23:23:21 +0900

    feat: move starship settings to a separate TOML file

--- home/cli/zsh/default.nix
+++ home/cli/zsh/default.nix
@@ -27,67 +27,6 @@
 
   programs.starship = {
     enable = true;
-    settings = builtins.fromTOML ''
-      format = """
-      $username\
-      $hostname\
-      $directory\
-      $git_branch\
-      $git_state\
-      $git_status\
-      $cmd_duration\
-      $line_break\
-      $python\
-      $nix_shell\
-      $character"""
-
-      [directory]
-      read_only = " "
-      style = "blue"
-
-      [character]
-      success_symbol = "[❯](purple)"
-      error_symbol = "[❯](red)"
-      vimcmd_symbol = "[❮](green)"
-
-      [git_branch]
-      format = "[$branch]($style)"
-      style = "bright-black"
-
-      [git_status]
-      format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)"
-      style = "cyan"
-      conflicted = "​"
-      untracked = "​"
-      modified = "​"
-      staged = "​"
-      renamed = "​"
-      deleted = "​"
-      stashed = "≡"
-
-      [git_state]
-      format = '\([$state( $progress_current/$progress_total)]($style)\) '
-      style = "bright-black"
-
-      [cmd_duration]
-      format = "[$duration]($style) "
-      style = "yellow"
-
-      [python]
-      format = "[$virtualenv]($style) "
-      style = "bright-black"
-
-      [nix_shell]
-      format = "[$name]($style) "
-      style = "bright-black"
-
-      [username]
-      format = "[$user]($style)"
-      style_user = "bright-black"
-
-      [hostname]
-      format = '[@$hostname]($style) '
-      style = "bright-black"
-    '';
+    settings = builtins.fromTOML (builtins.readFile ./starship_settings.toml);
   };
 }
