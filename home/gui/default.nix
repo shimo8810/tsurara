9897930ccacd422b94e715a42cb804787c48ce95
shimo8810 <shimo.8810@gmail.com>
2025-11-13 23:17:17 +0900

    build: update postman package configuration

--- home/gui/default.nix
+++ home/gui/default.nix
@@ -1,14 +1,23 @@
 { config, pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    # brave
-    # firefox
-    # vscode
-    # discord
-    # slack
-    # (config.lib.nixGL.wrap obsidian)
-    # (config.lib.nixGL.wrap postman)
-  ];
+  home.packages =
+    with pkgs;
+    [
+      # brave
+      # firefox
+      # vscode
+      # discord
+      # slack
+      # (config.lib.nixGL.wrap obsidian)
+      # postman
+      (writeShellScriptBin "postman-nosandbox" ''
+        exec ${config.lib.nixGL.wrap postman}/bin/postman --no-sandbox "$@"
+      '')
+
+    ]
+    ++ [
+      (config.lib.nixGL.wrap postman)
+    ];
 
   imports = [
     ./alacritty
