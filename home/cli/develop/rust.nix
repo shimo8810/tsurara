0332d3091b62b4e739232e3b4fa14550e12ef8d0
shimo8810 <shimo.8810@gmail.com>
2025-12-26 21:02:52 +0900

    build: comment out unused home.packages configuration

--- home/cli/develop/rust.nix
+++ home/cli/develop/rust.nix
@@ -1,9 +1,12 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    (fenix.combine [
-      fenix.stable.toolchain
-      fenix.targets.wasm32-unknown-unknown.stable.rust-std
-    ])
-  ];
+  # home.packages = with pkgs; [
+  #   (fenix.combine [
+  #     fenix.stable.toolchain
+  #     fenix.targets.wasm32-unknown-unknown.stable.rust-std
+  #   ])
+  # ];
+  programs.cargo = {
+    enable = true;
+  };
 }
