258313186882e2ea24086b7f263fbeaee2c52957
shimo8810 <shimo.8810@gmail.com>
2025-12-26 21:27:38 +0900

    build: uncomment home packages configuration in rust.nix

--- home/cli/develop/rust.nix
+++ home/cli/develop/rust.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }:
 {
-  # home.packages = with pkgs; [
-  #   (fenix.combine [
-  #     fenix.stable.toolchain
-  #     fenix.targets.wasm32-unknown-unknown.stable.rust-std
-  #   ])
-  # ];
+  home.packages = with pkgs; [
+    (fenix.combine [
+      fenix.stable.toolchain
+      fenix.targets.wasm32-unknown-unknown.stable.rust-std
+    ])
+  ];
   programs.cargo = {
     enable = true;
   };
