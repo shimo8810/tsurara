1cf28c0f8483cb2cd26764dd56005363bdd74702
shimo8810 <shimo.8810@gmail.com>
2026-01-06 20:00:47 +0900

    build: comment out unused packages in default.nix

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -1,17 +1,17 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    nordic
-    candy-icons
-    tela-circle-icon-theme
-    orchis-theme
-    graphite-gtk-theme
-    tokyonight-gtk-theme
-    zafiro-icons
-    nordzy-icon-theme
-    nordzy-cursor-theme
-    vimix-cursors
+    # nordic
+    # candy-icons
+    # tela-circle-icon-theme
+    # orchis-theme
+    # graphite-gtk-theme
+    # tokyonight-gtk-theme
+    # zafiro-icons
+    # nordzy-icon-theme
+    # nordzy-cursor-theme
+    # vimix-cursors
     hackgen-nf-font
-    capitaine-cursors-themed
+    # capitaine-cursors-themed
   ];
 }
