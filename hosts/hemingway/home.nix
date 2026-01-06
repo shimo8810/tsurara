6e15dcee4444734e6fb30252ea7bccf28eec075e
shimo8810 <shimo.8810@gmail.com>
2026-01-06 20:03:30 +0900

    build: remove unused nixGL configuration for genericLinux

--- hosts/hemingway/home.nix
+++ hosts/hemingway/home.nix
@@ -11,13 +11,14 @@
 
   targets.genericLinux = {
     enable = true;
-
-    nixGL = {
-      packages = inputs.nixgl.packages.${pkgs.stdenv.hostPlatform.system};
-      defaultWrapper = "nvidia";
-      installScripts = [
-        "nvidia"
-      ];
+    nixGL.packages = null;
+    gpu = {
+      enable = true;
+      nvidia = {
+        enable = true;
+        version = "580.95.05";
+        sha256 = "sha256-hJ7w746EK5gGss3p8RwTA9VPGpp2lGfk5dlhsv4Rgqc=";
+      };
     };
   };
 
