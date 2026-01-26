99ee2a5b112f20a63a691aec55c6afc2c3c9d9ec
shimo8810 <shimo.8810@gmail.com>
2026-01-26 22:43:55 +0900

    style(zellij): update zellij configuration and improve comments

--- home/modules/cli/zellij/default.nix
+++ home/modules/cli/zellij/default.nix
@@ -1,14 +1,15 @@
 { ... }:
+let
+  in
 {
-  # zellij configuration
   programs.zellij = {
     enable = true;
-    # enableZshIntegration = true;
+
     settings = {
       theme = "tokyo-night-storm";
       show_startup_tips = false;
       default_layout = "compact";
-      # copy_clipboard = "primary";
+      pane_frames = false;
       copy_command = "xclip -selection clipboard";
     };
   };
