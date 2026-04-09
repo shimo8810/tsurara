be83e021799a60aaa1e0b7e7009f7860d0065ece
shimo8810 <shimo.8810@gmail.com>
2026-04-09 20:41:17 +0900

    feat(ghostty): add settings for theme, background, font, and cursor

--- home/modules/apps/ghostty.nix
+++ home/modules/apps/ghostty.nix
@@ -1,8 +1,21 @@
-{ config, pkgs, ... }:
+{ ... }:
 {
   programs.ghostty = {
     enable = true;
     enableFishIntegration = true;
     enableZshIntegration = true;
+    settings = {
+      theme = "TokyoNight";
+      background = "#000000";
+      background-opacity = 0.85;
+
+      font-family = "HackGen Console NF";
+      font-size = 16;
+
+      cursor-style = "bar";
+      cursor-style-blink = "true";
+
+      copy-on-select = true;
+    };
   };
 }
