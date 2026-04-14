6636a1bc5377b6b48d945b029ff278f9dd82840e
shimo8810 <shimo.8810@gmail.com>
2026-04-14 21:55:36 +0900

    refactor(nixos): remove unused network proxy configuration and update locale settings

--- hosts/poe/nixos.nix
+++ hosts/poe/nixos.nix
@@ -7,7 +7,6 @@
 
 {
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
     inputs.xremap-flake.nixosModules.default
     inputs.vscode-server.nixosModules.default
@@ -21,10 +20,6 @@
   networking.hostName = "poe"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Enable networking
   networking.networkmanager.enable = true;
 
@@ -32,7 +27,8 @@
   time.timeZone = "Asia/Tokyo";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "ja_JP.UTF-8";
+  # i18n.defaultLocale = "ja_JP.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "ja_JP.UTF-8";
