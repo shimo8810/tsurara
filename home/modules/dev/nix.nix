f2e637c59e3236e92ea5a87cec058ce8bc611db5
shimo8810 <shimo.8810@gmail.com>
2026-05-16 08:56:37 +0900

    build: add configuration generation for nixd in home.activation

--- home/modules/dev/nix.nix
+++ home/modules/dev/nix.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, lib, ... }:
 {
   home.packages = with pkgs; [
     nixd # nix LSP server
@@ -6,4 +6,23 @@
     nvd # nix version diff
     # nixfmt-tree
   ];
+
+  home.activation.generateNixdConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    flake_path="${config.home.homeDirectory}/tsurara"
+    _hostname=$(uname -n | cut -d. -f1)
+    if [ -d "$flake_path" ]; then
+      cat > "$flake_path/.nixd.json" <<EOF
+{
+  "nixpkgs": {
+    "expr": "import (builtins.getFlake \"$flake_path\").inputs.nixpkgs { }"
+  },
+  "options": {
+    "home-manager": {
+      "expr": "(builtins.getFlake \"$flake_path\").homeConfigurations.\"${config.home.username}@$_hostname\".options"
+    }
+  }
+}
+EOF
+    fi
+  '';
 }
