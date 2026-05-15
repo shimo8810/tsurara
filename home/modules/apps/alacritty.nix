c6d7aa40c39cd7659b371bb7f585f375b1d7f97e
shimo8810 <shimo.8810@gmail.com>
2026-05-16 08:04:08 +0900

    build: update alacritty configuration for improved appearance and functionality

--- home/modules/apps/alacritty.nix
+++ home/modules/apps/alacritty.nix
@@ -3,16 +3,19 @@
 
   programs.alacritty = {
     enable = true;
+
+    theme = "tokyo_night";
+
     settings = {
       window = {
-        opacity = 0.5;
+        opacity = 0.85;
         blur = false;
       };
 
       font = {
-        size = 10;
+        size = 14;
         normal = {
-          family = "HackGen35 Console NF";
+          family = "HackGen Console NF";
           style = "Regular";
         };
       };
@@ -21,28 +24,6 @@
         primary = {
           background = "#0A0C0F";
           foreground = "#D8DEE9";
-        };
-
-        normal = {
-          black = "#3B4252";
-          red = "#BF616A";
-          green = "#A3BE8C";
-          yellow = "#EBCB8B";
-          blue = "#81A1C1";
-          magenta = "#B48EAD";
-          cyan = "#88C0D0";
-          white = "#E5E9F0";
-        };
-
-        bright = {
-          black = "#4C566A";
-          red = "#BF616A";
-          green = "#A3BE8C";
-          yellow = "#EBCB8B";
-          blue = "#81A1C1";
-          magenta = "#B48EAD";
-          cyan = "#8FBCBB";
-          white = "#ECEFF4";
         };
       };
 
